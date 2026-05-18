8e0ccbdda213bb6fd377841edee94487825336de
Himesh Ganorkar <himeshganorkar9@gmail.com>
2026-05-18 11:44:32 +0530

    Create lab1.sh

---
+++ 2026 May/SkillUpSummer/lab1.sh
@@ -0,0 +1,7 @@
+## GSP016 : Networking 101
+
+```
+curl -O https://raw.githubusercontent.com/PerkVerse/google-cloud-arcade/refs/heads/main/2026/Skill%20Up%20Summer/lab1.sh
+sudo chmod +x lab1.sh
+./lab1.sh
+```
